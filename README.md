2b24641d2dea060693b5ba96f7cac800e418548f
camilo <camilo.tapia@gmail.com>
2020-11-04 14:36:30 +0100

    remove unneeded dependencies, rewrite some more, add config, add predefined apps

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ nodejs 12+
 
 ## How to use
 
+Install it globally
+
+```npm install -g i3-keyboard-input-switcher```
+
 Edit your ```~/.config/i3/config``` and add the following line
 
 ```
@@ -16,3 +20,20 @@ exec --no-startup-id i3-keyboard-switcher us se
 
 # Add a bindind for switching
 bindsym $mod+space exec i3-keyboard-switcher switch
+```
+
+## Advanced config
+
+If you wich to configurate the switcher a little more, place a ```config.json``` file in ```$HOME/.config/i3-keyboard-switcher``` with:
+
+```json
+{
+    "port": 21111, // Custom port, defaults to 18888
+    "inputs": ["us", "se"], // Inputs, instead of adding them as arguments in i3 config
+    "predefined": {
+        "gnome-terminal-server": "us" // Predefined apps that should have a specific input
+    },
+    "logFile": "/home/camilo/testlog.log", // Custom logifle path
+    "log": "console" // false || true || console. If false, nothing is logged, if true we use the file. If console, we use the normal nodejs console.log
+}
+```
--- index.js
+++ index.js
@@ -2,44 +2,97 @@ const exec = require('child_process').exec;
 const i3 = require('i3');
 
 const http = require('http');
-const fetch = require('node-fetch');
 const path = require('path');
 const fs = require('fs');
 
-const log = (message) => {
-    if (process.env.DEBUG_I3) {
-        fs.appendFileSync(path.join(__dirname, './test.log'), `${message}\r\n`);
-    }
-};
-
+const appPath = (path.join(process.env.HOME, './.config/i3-keyboard-switcher'));
 const current = {};
 let currentInstance = '';
 let index = 0;
 
+let defaults = {
+    port: 18888,
+    inputs: ['us', 'se'],
+    predefined: { },
+    logFile: '/var/log/i3-keyboard-switcher.log',
+    log: 'console',
+};
+
+let config = {};
+
+try {
+    config = {
+        ...defaults,
+        ...require(path.join(appPath, './config.json')),
+    };
+} catch (_) {
+    config = defaults;
+}
+
+const log = (message) => {
+    if (config.log) {
+        if (config.log === 'console') {
+            console.log(message);
+        } else {
+            fs.appendFileSync(config.logFile, `${message}\r\n`);
+        }
+    }
+};
+
 const setKeyboardLayout = (layout = 'us') => new Promise((resolve, reject) => {
     exec(`setxkbmap ${layout}`, function(err, stdout, stderr) {
         if (err) {
-            console.log(err);
             return resolve(false);
         }
         if (stderr) {
-            console.log(stderr);
             return resolve(false);
         }
         return resolve(true);
     });
 });
 
+const switchLayout = async (layout = '') => new Promise((resolve, reject) => {
 
-const switchLayout = async (layout = '') => {
-    return await fetch(`http://127.0.0.1:18888?layout=${layout}`);
-};
+    const callback = (response) => {
+        let done = false;
+        let body = '';
+        response.on('data', function (chunk) {
+            body += chunk;
+        });
+        response.on('end', function () {
+            if (!done) {
+                done = true;
+                resolve(body);
+            }
+        });
+        response.on('error', function () {
+            if (!done) {
+                done = true;
+                reject(body);
+            }
+        });
+    };
+
+    http
+        .request({
+            method: 'GET',
+            host: '127.0.0.1',
+            port: config.port,
+            path: `/?layout${layout}`,
+        }, callback)
+        .end();
+
+});
 
 const start = (layoutList) => {
 
+    if (layoutList.length === 0 && config.inputs) {
+        layoutList = config.inputs;
+    }
+
     const i3Client = i3.createClient();
 
-    const requestListener = async (req, res) => {
+    const requestListener = (req, res) => {
         const layoutRe = /layout=(\w\w)/;
         const match = layoutRe.exec(req.url);
         let layout = '';
@@ -52,38 +105,39 @@ const start = (layoutList) => {
             }
             layout = layoutList[index];
         }
-        const ok = await setKeyboardLayout(layout);
-        if (ok) {
-            if (currentInstance) {
-                console.log('remember ', currentInstance, layout);
-                current[currentInstance] = layout;
+        setImmediate(async () => {
+            const ok = await setKeyboardLayout(layout);
+            if (ok) {
+                if (currentInstance) {
+                    log(`remember ${currentInstance} ${layout}`);
+                    current[currentInstance] = layout;
+                }
+                return res.end('ok -- ' + layout + ' ' + index);
             }
-            log('----' + ok);
-            return res.end('ok -- ' + layout + ' ' + index);
-        }
-        log('errroooorr');
-        res.end('error');
+            res.end('error');
+        });
     };
 
     const server = http.createServer(requestListener);
-    server.listen(18888);
+    server.listen(config.port);
 
     i3Client.on('window', function(e) {
         if (e.container.focused) {
             currentInstance = e.container.window_properties.instance;
-            if (current[currentInstance]) {
-                console.log('remember ', currentInstance, current[currentInstance]);
+            if (config.predefined && config.predefined[currentInstance]) {
+                log(`predefined ${currentInstance} ${config.predefined[currentInstance]}`);
+                setKeyboardLayout(config.predefined[currentInstance]);
+             } else if (current[currentInstance]) {
+                log(`remember ${currentInstance} ${current[currentInstance]}`);
                 setKeyboardLayout(current[currentInstance]);
             }
         }
     });
 
-    console.log('start with', layoutList.join(' & '));
-    log('start');
+    log('start with ' + layoutList.join(' & ') + ' on port ' + config.port);
 
     process.on( 'SIGTERM', function () {
         server.close(function () {
-            console.log("Finished all requests");
             process.exit(0);
         });
     });
@@ -92,4 +146,4 @@ const start = (layoutList) => {
 
 exports.start = start;
 exports.switchLayout = switchLayout;
-
+exports.log = log;
